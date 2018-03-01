e3c773e80eb5bae28fc015cc55e6c5f1c8e8ee80
Mary Kathryn Syms <syms@ladytacos.com>
2018-03-01 16:04:21 -0600

    changed dependency issues and Springboot version

--- src/main/java/com/sideworksa/demo/DemoApplication.java
+++ src/main/java/com/sideworksa/demo/DemoApplication.java
@@ -2,11 +2,6 @@ package com.sideworksa.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-<<<<<<< Updated upstream
-=======
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
->>>>>>> Stashed changes
 
 @SpringBootApplication
 public class DemoApplication {
