ab5788a41606022915abfe8dad8960a4f0a41e47
Mary Kathryn Syms <syms@ladytacos.com>
2018-03-01 15:16:26 -0600

    finished user table. created getters, setters, and construructors

--- src/main/java/com/sideworksa/demo/models/User.java
+++ src/main/java/com/sideworksa/demo/models/User.java
@@ -7,5 +7,61 @@ import javax.persistence.*;
 @Table(name = "users")
 public class User {
 
+    // id
+    @Id
+    @GeneratedValue
+    private long id;
+
+    // username
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    // email
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    // password
+    @Column(nullable = false)
+    private String password;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
 
 }
