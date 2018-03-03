8201934d8f702eaf9ba7919918d7687b5a7a6223
Mary Kathryn Syms <syms@ladytacos.com>
2018-03-03 10:48:09 -0600

    added postmapping and password encoder to business controller

--- src/main/java/com/sideworksa/demo/controllers/BusinessesController.java
+++ src/main/java/com/sideworksa/demo/controllers/BusinessesController.java
@@ -4,27 +4,23 @@ import com.sideworksa.demo.models.Business;
 import com.sideworksa.demo.models.User;
 import com.sideworksa.demo.repositories.BusinessRepository;
 import com.sideworksa.demo.repositories.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class BusinessesController {
     private final BusinessRepository businessRepository;
     private final UserRepository userRepository;
-//    private final PasswordEncoder passwordEncoder;
-//
-//    public BusinessesController(BusinessRepository businessRepository, UserRepository userRepository, PasswordEncoder passwordEncoder) {
-//        this.businessRepository = businessRepository;
-//        this.userRepository = userRepository;
-//        this.passwordEncoder = passwordEncoder;
-//    }
+    private final PasswordEncoder passwordEncoder;
 
-
-    // delete after authentication passwordEncoder
-    public BusinessesController(BusinessRepository businessRepository, UserRepository userRepository) {
+    public BusinessesController(BusinessRepository businessRepository, UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.businessRepository = businessRepository;
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @GetMapping("/businesses/create")
@@ -35,17 +31,17 @@ public class BusinessesController {
         return "businesses/create";
     }
 
-//    @PostMapping("/businesses/create")
-//    public String saveNewBusiness(@ModelAttribute User user, @ModelAttribute Business business) {
-//        String hash = passwordEncoder.encode(user.getPassword());
-//
-//        user.setPassword(hash);
-//        userRepository.save(user);
-//        business.setUser(user);
-//        user.setBusiness(business);
-//        businessRepository.save(business);
-//
-//        return "redirect:/login";
-//    }
+    @PostMapping("/businesses/create")
+    public String saveNewBusiness(@ModelAttribute User user, @ModelAttribute Business business) {
+        String hash = passwordEncoder.encode(user.getPassword());
+
+        user.setPassword(hash);
+        userRepository.save(user);
+        business.setUser(user);
+        user.setBusiness(business);
+        businessRepository.save(business);
+
+        return "redirect:/login";
+    }
 
 }
