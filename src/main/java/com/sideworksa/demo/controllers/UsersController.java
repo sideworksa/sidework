c3dda3065d6dd7484a44b3728c146bb9341915e6
Mary Kathryn Syms <syms@ladytacos.com>
2018-03-06 13:25:55 -0600

    edited dependencies in pom.xml

--- src/main/java/com/sideworksa/demo/controllers/UsersController.java
+++ src/main/java/com/sideworksa/demo/controllers/UsersController.java
@@ -1,20 +1,20 @@
 package com.sideworksa.demo.controllers;
 
-import com.sideworksa.demo.models.User;
 import com.sideworksa.demo.repositories.UserRepository;
+import com.sideworksa.demo.repositories.Users;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 
 
 @Controller
 public class UsersController {
-    private User user;
+    private Users users;
     private UserRepository userRepository;
     private PasswordEncoder passwordEncoder;
-    public UsersController(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+
+    public UsersController(Users users, UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.users = users;
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
     }
-
-
 }
