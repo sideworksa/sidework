90e34d2dea73079c57ecee20377953b175b12563
Mary Kathryn Syms <syms@ladytacos.com>
2018-09-08 18:12:13 -0500

    user can edit password and password persists. watch for business account redirect. no validation yet

--- src/main/java/com/sideworksa/demo/controllers/UsersController.java
+++ src/main/java/com/sideworksa/demo/controllers/UsersController.java
@@ -1,14 +1,17 @@
 package com.sideworksa.demo.controllers;
 
+import com.sideworksa.demo.models.User;
 import com.sideworksa.demo.repositories.BusinessRepository;
 import com.sideworksa.demo.repositories.UserRepository;
 import com.sideworksa.demo.repositories.WorkerRepository;
 import com.sideworksa.demo.services.BusinessService;
 import com.sideworksa.demo.services.WorkerService;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
@@ -35,6 +38,24 @@ public class UsersController {
     @GetMapping("/users/password")
     public String showChangePasswordForm(Model vModel) {
         return "users/password";
+    }
+
+    @PostMapping("/users/password")
+    public String changePassword(@RequestParam(name = "current_password") String current_password,
+                                 @RequestParam(name = "new_password") String new_password,
+                                 @RequestParam(name = "confirm_password") String confirm_password) {
+        // save new password to logged-in user
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (user.getId() == 0) {
+            return "redirect:/login";
+        }
+        user = userRepository.findById(user.getId());
+        String hash = passwordEncoder.encode(new_password);
+        user.setPassword(hash);
+        workerService.save(user);
+
+        return "redirect:/workers/profile";
+        // keep in mind, might have to handle business change password
     }
 
     @GetMapping("/workers/search")
